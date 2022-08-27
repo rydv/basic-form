d24d4df94c11d74960e8951702f336d33074e5f2
Sanjay Yadav <sanjayyadav@Sanjays-MBP.bbrouter>
2022-08-27 16:51:10 +0530

    Adding Code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import {React, useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  const [success, setSuccess] = useState(false)
+  const [submitted, setSubmitted] = useState(false)
+
+  // useEffect(() => {
+  //   setValues({
+  //     firstName: "",
+  //     lastName: "",
+  //     email: "",
+  //   })
+  //   setSuccess(false)
+  //   setSubmitted(false)
+  // })
+
+  const handleFirstNameInputChange = (event) =>{
+    setValues({...values, firstName: event.target.value})
+    setSuccess(false)
+  }
+
+  const handleLastNameInputChange = (event) =>{
+    setValues({...values, lastName: event.target.value})
+    setSuccess(false)
+  }
+
+  const handleEmailInputChange = (event) =>{
+    setValues({...values, email: event.target.value})
+    setSuccess(false)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(values.firstName && values.lastName && values.email) {
+      setSuccess(true)
+    }
+    setSubmitted(true)
+    console.log(values)
+    
+  }
+   
+   return (
+
+    <div className="form-container">
+      <form className="register-form" onSubmit={handleSubmit}>
+        {success ? <div className="success-message">Success! Thank you for registering.</div> : null}
+      <input
+      onChange={handleFirstNameInputChange}
+      value={values.firstName}
+      className="form-field" 
+      placeholder="First Name"
+      name="firstName" />
+      {submitted && !values.firstName ? <span>Please enter first name</span> : null}
+      
+      <input
+      onChange={handleLastNameInputChange} 
+      value={values.lastName}
+      className="form-field" 
+      placeholder="Last Name"
+      name="lastName"/>
+      {submitted && !values.lastName ? <span>Please enter last name</span> : null}
+      <input 
+      onChange={handleEmailInputChange}
+      value={values.email}
+      className="form-field" 
+      placeholder="Email"
+      name="email"/>
+      {submitted && !values.email ? <span>Please enter email</span> : null}
+      <button 
+      className="form-field"
+      >Register</button>
+      </form>
     </div>
   );
 }
-
-export default App;
